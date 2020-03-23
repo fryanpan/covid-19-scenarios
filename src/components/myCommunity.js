16db84c80feaa0391161a49bab1a92280fe59d05
Bryan Chan <fryanpan@gmail.com>
2020-03-22 19:20:39 -0700

    Updating community charts

--- src/components/myCommunity.js
+++ src/components/myCommunity.js
@@ -1,11 +1,18 @@
 import React from "react"
 
 import {
-    BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+    AreaChart, Area, BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
   } from 'recharts';
+
+import moment from 'moment';
   
 
 class MyCommunity extends React.Component {    
+    formatDateAxis(tickItem) {
+        console.log(tickItem);
+        return moment(tickItem).toString("YYYY-MM-DD");
+    }
+
     render() {        
         console.log("Rendering charts", this.props.modelData);
         return <div>
@@ -14,23 +21,41 @@ class MyCommunity extends React.Component {
             </h1>
 
             <h2>How many people might be exposed?</h2>
-            <BarChart
-                width={500}
+            <AreaChart
+                width={960}
                 height={300}
                 data={this.props.modelData}
                 margin={{
-                top: 20, right: 30, left: 20, bottom: 5,
+                    top: 10, right: 30, left: 0, bottom: 0,
                 }}
             >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="date" />
+                <XAxis dataKey="date" tickFormatter={this.formatDateAxis}/>
                 <YAxis />
                 <Tooltip />
                 <Legend />
-                <Bar dataKey="exposed" stackId="a" fill="#8884d8" />
-                <Bar dataKey="infected" stackId="a" fill="#82ca9d" />
-                <Bar dataKey="recovered" stackId="a" fill="#8884d8" />
-                <Bar dataKey="dead" stackId="a" fill="#8884d8" />
+                <Area type="monotone" dataKey="recovered"  fill="#8884d8" />
+                <Area type="monotone" dataKey="infected"  fill="#82ca9d" />
+                <Area type="monotone" dataKey="exposed" fill="#8884d8" />
+                <Area type="monotone" dataKey="dead" fill="#8884d8" />
+            </AreaChart>
+
+            <h2>How many people might die?</h2>
+            <BarChart
+                width={960}
+                height={300}
+                data={this.props.modelData}
+                margin={{
+                    top: 10, right: 30, left: 0, bottom: 0,
+                }}
+                barCategoryGap={1}
+                barGap={0}
+            >
+                <XAxis dataKey="date" tickFormatter={this.formatDateAxis}/>
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar type="monotone" dataKey="confirmedDeaths" stackId="a" fill="#82ca9d" />
+                <Bar type="monotone" dataKey="dead" stackId="b" fill="#8884d8" />
             </BarChart>
         </div>
     }
